4aaccbc85100e923e27abda62ae0e87fa2da7c0f
laxmi <dlaksh74@gmail.com>
2024-01-30 17:00:49 +0530

    changes as vercel config

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 const express = require("express");
-const dbConnect = require("./db")
 
 const app = express();
-
-app.use(express.json());
-
 
 app.get("/", (req, res)=> {
     res.status(200).json({msg:"app is running successfully"})
 })
 const port = process.env.PORT || 8000
-
-//connection with db
-dbConnect();
 
 app.listen(port, ()=>{
     console.log(`app is running in http://localhost:${port}`)
