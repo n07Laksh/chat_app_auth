241c1f98e5877b75b6f8a7e32cae4eb43a1c0fff
laxmi <dlaksh74@gmail.com>
2024-02-18 17:23:19 +0530

    Revert "Merge pull request #4 from n07Laksh/prifile_utility"
    This reverts commit

--- index.js
+++ index.js
@@ -17,16 +17,9 @@ app.use(
 );
 
 app.get("/", (req, res) => {
-<<<<<<< HEAD
-<<<<<<< HEAD
   res.status(200).json({ message: "My chat app is running successfully" });
-=======
-  res.status(200).json({ msg: "app is running successfully" });
->>>>>>> parent of 3e224f5 (Merge pull request #4 from n07Laksh/prifile_utility)
-=======
-  res.status(200).json({ msg: "app is running successfully" });
->>>>>>> parent of 3e224f5 (Merge pull request #4 from n07Laksh/prifile_utility)
-});
+})
+
 const port = process.env.PORT || 8000;
 
 app.use("/chatapp/user/auth", require("./Routes/auth"));
