d1afd2e44eb42f4cd4f7f08ffe956aa7fa0a3b14
laxmi <dlaksh74@gmail.com>
2024-02-18 17:20:18 +0530

    Revert "Merge pull request #4 from n07Laksh/prifile_utility"
    This reverts commit 3e224f5b6d6ea36b055d930a7dd0b54714a7468b, reversing
    changes made to dfed3f063afa8945d9503c10938fc2fa949d19b3.

--- index.js
+++ index.js
@@ -18,7 +18,11 @@ app.use(
 
 app.get("/", (req, res) => {
 <<<<<<< HEAD
+<<<<<<< HEAD
   res.status(200).json({ message: "My chat app is running successfully" });
+=======
+  res.status(200).json({ msg: "app is running successfully" });
+>>>>>>> parent of 3e224f5 (Merge pull request #4 from n07Laksh/prifile_utility)
 =======
   res.status(200).json({ msg: "app is running successfully" });
 >>>>>>> parent of 3e224f5 (Merge pull request #4 from n07Laksh/prifile_utility)
