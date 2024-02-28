1365cbbfdb4521eb36f46d5df81cef0f60962e53
laxmi <dlaksh74@gmail.com>
2024-02-28 11:06:01 +0530

    cookie secure for development set false

--- UtilityFunction/utilityFunction.js
+++ UtilityFunction/utilityFunction.js
@@ -1,20 +1,17 @@
-
-
-
 function sendCookie(res, key, value, expDay) {
-    const exDate = new Date(Date.now() + expDay * 24 * 60 * 60 * 1000);
+  const exDate = new Date(Date.now() + expDay * 24 * 60 * 60 * 1000);
 
-    const cookieOptions = {
-        path: "/",
-        expires: exDate,
-        secure: false,
-        httpOnly: true,
-    }
+  const cookieOptions = {
+    path: "/",
+    expires: exDate,
+    secure: false,
+    httpOnly: true,
+    SameSite: "None",
+  };
 
-    if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
-
-    res.cookie(key, value, cookieOptions);
+  // if (process.env.NODE_ENV === "production") cookieOptions.secure = true; // enable after client app is deployed in secure ssl
+  // https to http is not work while we assign secure true
+  res.cookie(key, value, cookieOptions);
 }
 
-
-module.exports = { sendCookie }
+module.exports = { sendCookie };
