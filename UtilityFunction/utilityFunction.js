2f39c4f6a2576cdec564bb8424297bdb373ca19c
Laxmi Lal <112006645+n07Laksh@users.noreply.github.com>
2024-03-01 14:24:47 +0530

    Update utilityFunction.js *.vercel.app

--- UtilityFunction/utilityFunction.js
+++ UtilityFunction/utilityFunction.js
@@ -9,7 +9,7 @@ function sendCookie( res, key, value ) {
   expiryDate.setDate(expiryDate.getDate() + parseInt(expDay)); // Expires in 100 days
 
   var setCookie = cookie.serialize(key, String(value), {
-    domain:".vercel.app",
+    domain:"*.vercel.app",
     path: "/",
     httpOnly: true,
     expires: expiryDate, // Use the Date object here
